21400a9d0849bd1b039574a12e1b6cf19c443cc7
carlosepmendes <carlosepmendes@gmail.com>
2015-09-21 17:19:32 +0000

    1.2 Edit Text

--- app/src/main/java/com/example/android/lab/MainActivity.java
+++ app/src/main/java/com/example/android/lab/MainActivity.java
@@ -4,14 +4,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText eText;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eText = (EditText) findViewById(R.id.edittext);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String str = eText.getText().toString();
+                Toast msg = Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG);
+                msg.show();
+            }
+        });
     }
 
 
