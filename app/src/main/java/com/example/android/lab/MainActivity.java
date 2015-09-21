8ac27fda15e40eff6ff26fb2176a3fdb65eeac76
carlosepmendes <carlosepmendes@gmail.com>
2015-09-21 17:43:53 +0000

    1.3 Arrays

--- app/src/main/java/com/example/android/lab/MainActivity.java
+++ app/src/main/java/com/example/android/lab/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.android.lab;
 
+import android.os.Debug;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,41 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //cria um array com 5 elementos
+        int[] myArray = new int[5];
+        //popula as 5 posições do array, o último elemento é:  "size" -1
+        myArray[0] = 10;
+        myArray[1] = 20;
+        myArray[2] = 30;
+        myArray[3] = 40;
+        myArray[4] = 50;
+
+        //outra forma de declarar
+        int[] outroArray = {10, 20, 30, 40, 50};
+        //e agora mandar para a consola
+        Log.v("LogExample", String.valueOf(outroArray[0]));
+        //log levels : V: Verbose   D: Debug  W: Warning  I: Info  E: Error
+
+        //imprimir todas as linhas
+        Log.v("LogExample", String.valueOf(myArray[0]));
+        Log.v("LogExample", String.valueOf(myArray[1]));
+        Log.v("LogExample", String.valueOf(myArray[2]));
+        Log.v("LogExample", String.valueOf(myArray[3]));
+        Log.v("LogExample", String.valueOf(myArray[4]));
+
+        //ou com mais classe:
+        for (int i = 0; i < myArray.length; i++) {
+            Log.v("LogExample", String.valueOf(myArray[i]));
+        }
+
+        //ou com um for mais simples:
+        for (int i : myArray) {
+            Log.v("LogExample", String.valueOf(i));
+        }
+
+        //ou com um utilitário:
+        Log.v("LogExample", Arrays.toString(myArray));
     }
 
 
