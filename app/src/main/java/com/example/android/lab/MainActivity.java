c1f9e5baf7a93800c71bc560a69471996a9f80e0
carlosepmendes <carlosepmendes@gmail.com>
2015-09-22 11:48:44 +0000

    1.4 Autocomplete

--- app/src/main/java/com/example/android/lab/MainActivity.java
+++ app/src/main/java/com/example/android/lab/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +15,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String str[]={"Arun","Mathev","Vishnu","Vishal","Arjun",
+                "Arul","Balaji","Babu","Boopathy","Godwin","Nagaraj"};
+
+        AutoCompleteTextView t1 = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
+
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,str);
+
+        t1.setThreshold(1);
+        t1.setAdapter(adp);
+
     }
 
 
