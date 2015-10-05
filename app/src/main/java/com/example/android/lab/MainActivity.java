535da179c67bcd8291cc81fa7c28b8f6b38fcefd
CarlosMendes <carlosepmendes@gmail.com>
2015-10-05 22:04:37 +0000

    3.1 onLongPress

--- app/src/main/java/com/example/android/lab/MainActivity.java
+++ app/src/main/java/com/example/android/lab/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.lab;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,25 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtView = (TextView) findViewById(R.id.txtView);
+        txtView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(),
+                        "You have pressed it long :)", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+        txtView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(), "Not Long Enough :(",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
